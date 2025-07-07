ff0bb854e5f1e9b4923260063c810620422e3221
Duarte, Wilson Beirigo <wilson.duarte@aecom.com>
2025-07-07 18:36:50 -0300

    Ajustes url geoserver/catalog

--- app/main.py
+++ app/main.py
@@ -3,9 +3,6 @@ import os
 import uuid
 from typing import List
 
-
-
-
 from fastapi import FastAPI, UploadFile
 import os
 from dotenv import dotenv_values, load_dotenv
@@ -14,10 +11,6 @@ import app.src.config as settings
 from app.src.main_task import main
 
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to FastAPI!"}
 
 
 @app.post("/uploadfiles/")
--- app/src/config.py
+++ app/src/config.py
@@ -9,18 +9,14 @@ ENV: str = "dev"
 VALID_FILES: Tuple[str, ...] = (".gpkg", ".xml", ".xlsx")
 
 ##### GEOSERVER
-#GEOSERVER_URL:str = 'https://gis.iocasta.com.br/geoserver'
-
-GEOSERVER_URL:str = 'http://cobalto.iocasta.com.br:8080/geoserver'
+GEOSERVER_URL:str = 'https://gisdev.iocasta.com.br/geoserver'
 
 GEOSERVER_USER:str = 'admin'
 GEOSERVER_PASSWORD:str = 'geoserver'
 GEOSERVER_WORKSPACE:str = 'gold'
 
 ##### GEONETWORK
-# GEONETWORK_SERVER:str = 'https://catalog.iocasta.com.br'
-
-GEONETWORK_SERVER:str = 'http://cobalto.iocasta.com.br:8081/'
+GEONETWORK_SERVER:str = 'https://catalogdev.iocasta.com.br'
 GEONETWORK_USERNAME:str = 'admin'
 GEONETWORK_PASSWORD:str = 'admin'
 GEONETWORK_AUTH_URL = GEONETWORK_SERVER + "/srv/eng/info?type=me"
@@ -35,16 +31,3 @@ def init_dir():
         logging.info(f"Creating directory {dir_name}")
         os.mkdir(dir_name)
 
-
-"""
-/home/vscode/data/new/file/BIO/especies_ameacadas/20240101/00/md_bio_sp_end_20240101.xml
-<gmd:dataQualityInfo>
-    <gmd:DQ_DataQuality>
-      <gmd:lineage>
-        <gmd:LI_Lineage>
-          <gmd:processStep>
-            <gmd:LI_ProcessStep>
-              <gmd:description>
-.//gmd:dataQualityInfo/gmd:DQ_DataQuality/gmd:lineage/gmd:LI_Lineage/gmd:processStep/gco:description
-
-"""
